1ec0cb49a838ebf2689c76700b8b702ba4b6ac9f
Nandhu <85504447+NandhuImprefcet@users.noreply.github.com>
2021-08-30 16:15:54 +0530

    Create Unique1.java

---
+++ Classroom Problems/Unique1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Unique1
+{
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        String word="";
+        for(int i=0;i<line.length();i++)
+        {
+            char let=line.charAt(i);
+            if((let==' ')||(let == '.'))
+            {
+                if(line.indexOf(word)==line.lastIndexOf(word))
+                    System.out.println(word);
+                word="";
+            }
+            else
+                word+=let;
+        }
+    }
+}

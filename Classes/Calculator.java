dfce2f6a8ca935e7702bcdc821bd273711394674
Nandhu <85504447+NandhuImprefcet@users.noreply.github.com>
2021-09-16 16:14:57 +0530

    Create Calculator.java

---
+++ Classes/Calculator.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Calc
+{
+    int add(int a,int b)
+    {
+        return a+b;
+    }
+    int sub(int a,int b)
+    {
+        return a-b;
+    }
+    int mul(int a,int b)
+    {
+        return a*b;
+    }
+    int div(int a, int b)
+    {
+        return a/b;
+    }
+    int mod(int a, int b)
+    {
+        return a%b;
+    }
+}
+public class Calculator
+{
+    public static void main(String []args)
+    {
+        Calc c = new Calc();
+        System.out.println(c.add(10,15));
+        System.out.println(c.sub(10,15));
+        System.out.println(c.mul(10,15));
+        System.out.println(c.div(10,15));
+        System.out.println(c.mod(10,15));
+    }
+}

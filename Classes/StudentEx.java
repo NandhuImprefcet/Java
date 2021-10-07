133f67f2f9fe3bc970a23b87c7240fc63c1e367c
Nandhu <85504447+NandhuImprefcet@users.noreply.github.com>
2021-10-07 13:49:09 +0530

    Create StudentEx.java

---
+++ Classes/StudentEx.java
@@ -0,0 +1,31 @@
+class Person
+{
+    void message()
+    {
+        System.out.println("This is person class");
+    }
+}
+
+class Student extends Person
+{
+    void message()
+    {
+        System.out.println("This is Student class");
+    }
+    void display()
+    {
+        message();
+    }
+}
+
+public class StudentEx
+{
+    public static void main(String []args)
+    {
+        Person p = new Person();
+        Student s = new Student();
+        p.message();
+        s.display();
+        s.message();
+    }
+}

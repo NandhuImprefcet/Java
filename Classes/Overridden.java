d8e1d47091d969998d86a93668c9fe3b6f0459da
Nandhu <85504447+NandhuImprefcet@users.noreply.github.com>
2021-10-09 11:44:30 +0530

    Create Overridden.java

---
+++ Classes/Overridden.java
@@ -0,0 +1,36 @@
+class Shapes
+{
+    public void area()
+    {
+        System.out.println("The formula or area of ");
+    }
+}
+
+class Triangle extends Shapes
+{
+    public void area()
+    {
+        System.out.println("Triangle is 1/2 * base * height");
+    }
+}
+
+class Circle extends Shapes
+{
+    public void area()
+    {
+        System.out.println("Circle is 3.14 * radius * radius");
+    }
+}
+
+public class Overridden
+{
+    public static void main(String []args)
+    {
+        Shapes shape = new Shapes();
+        Shapes tri = new Triangle();
+        Shapes cir = new Circle();
+        shape.area();
+        tri.area();
+        cir.area();
+    }
+}
